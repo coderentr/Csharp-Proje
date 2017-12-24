2e251a3c03346258084f9055a7b75bc098f350fe
Mustafa EREN <Mustafa EREN@DESKTOP-1ROBH45>
2017-12-25 00:03:07 +0300

    MvcLoginRegister

---
+++ MVCLoginRegister/MVCLoginRegister/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using MVCLoginRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLoginRegister.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using(OurDbContext db=new OurDbContext())
+            {
+                return View(db.UserAccount.ToList());
+            }
+        }
+        //Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using(OurDbContext db=new OurDbContext())
+                {
+                    db.UserAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.Ad + "  " + account.Soyad + "Kayıt Başarılı. ";
+            }
+
+            return View();
+        }
+        //Login
+
+
+
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(UserAccount User)
+        {
+            using(OurDbContext db=new OurDbContext())
+            {
+                var usr = (from k in db.UserAccount
+                           where k.KullaniciAdi == User.KullaniciAdi &&
+                           k.Sifre == User.Sifre
+                           select k).SingleOrDefault();
+                if(usr!= null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["KullaniciAdi"] = usr.KullaniciAdi.ToString();
+                    return RedirectToAction("GirisSayfasi");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifre yanlış.");
+                }
+            }
+            return View();
+        }
+        public ActionResult GirisSayfasi()
+        {
+            if (Session["UserID"]!= null)
+            {
+                   return View();
+            }
+            else
+            {
+              return   RedirectToAction("Giris");
+            }
+        }
+    }
+}
---
+++ MVCLoginRegister/MVCLoginRegister/Models/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLoginRegister.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="İsim bilginizi girmeniz gerekmektedir.")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage ="Soyisim bilginizi girmeniz gerekmektedir.")]
+        public string Soyad { get; set; }
+
+        [Required(ErrorMessage = "E-Mail bilginizi girmeniz gerekmektedir.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Lütfen geçerili bir e posta adresi giriniz.")]
+        public string E_Mail  { get; set; }
+        [Required(ErrorMessage ="Kulanıcı adınızı girmelisiniz.")]
+        public string  KullaniciAdi { get; set; }
+        [Required(ErrorMessage ="Sifrenizi girmelisiniz.")]
+        [DataType(DataType.Password)]
+        public string  Sifre { get; set; }
+        [Compare("Sifre",ErrorMessage ="Lütfen şifreyi tekrar giriniz.")]
+        [DataType(DataType.Password)]
+        public string SifreTekrar { get; set; }
+    }
+}
